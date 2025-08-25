1209eded360c8bed788cdb4e75b13bdd0e8c3bca
girumer <bgirum9@gmail.com>
2025-08-26 01:16:33 +0300

    Initial commit without large files

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,10 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
-// Change this to your backend server URL
-const socket = io("http://localhost:5000", {
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+const socket = io(BACKEND_URL, {
   withCredentials: true,
+  transports: ["websocket", "polling"]
 });
 
 export default socket;
