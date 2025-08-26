e7742ecab2006c627a9764b783d53e11e99386b6
girumer <bgirum9@gmail.com>
2025-08-26 06:08:14 +0300

    Initial commit without large files

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,10 @@ import { io } from "socket.io-client";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-const socket = io(BACKEND_URL, {
+// Determine WebSocket URL dynamically
+const SOCKET_URL = BACKEND_URL.replace(/^http/, 'ws'); // http -> ws, https -> wss
+
+const socket = io(SOCKET_URL, {
   withCredentials: true,
   transports: ["websocket", "polling"]
 });
