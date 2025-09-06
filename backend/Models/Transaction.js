b70430ad2c7c92949c73a2bc1c95c6ec9f80bf3a
girumer <bgirum9@gmail.com>
2025-09-07 00:24:17 +0300

    Initial commit without large files

--- backend/Models/Transaction.js
+++ backend/Models/Transaction.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-
 const transactionSchema = new mongoose.Schema({
   transactionNumber: { type: String, required: true, unique: true },
   phoneNumber: { type: String, required: true, default: "UNKNOWN" },
-  type: { type: String, enum: ["telebirr", "cbe"], required: true },
+  type: { type: String, enum: ["deposit", "withdrawal"], required: true },
   amount: { type: Number, required: true },
-  method:{type:String,enum:["deposit","withdrawal"],required:true},
-  rawMessage: { type: String }, // optional: store the full message
-  createdAt: { type: Date, default: Date.now }
-});
+  method: { type: String, enum: ["telebirr", "cbebirr"], required: true },
+  rawMessage: { type: String },
+  status: { type: String, enum: ["pending", "completed", "failed"], default: "pending" },
+  createdAt: { type: Date, default: Date.now },
+})
 
 module.exports = mongoose.model("Transaction", transactionSchema);
