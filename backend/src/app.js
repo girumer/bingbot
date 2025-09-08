008fcb81e84b0d659c9da8ef16cae268308983d3
girumer <bgirum9@gmail.com>
2025-09-08 03:38:25 +0300

    Initial commit without large files

--- backend/src/app.js
+++ backend/src/app.js
@@ -659,7 +659,7 @@ app.get("/admin/pending-withdrawals",  async (req, res) => {
 app.post("/admin/confirm-withdrawal", async (req, res) => {
   const {withdrawalId} = req.body;
   try {
-    const transaction = await Transaction.findById({withdrawalId});
+    const transaction = await Transaction.findByOne({withdrawalId});
     if (!transaction) {
       return res.status(404).json({ message: "Transaction not found." });
     }
