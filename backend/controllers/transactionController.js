e6833c140e48193a0d749ae46e15c7ae817fa439
girumer <bgirum9@gmail.com>
2025-09-14 20:42:27 +0300

    Initial commit without large files

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -26,7 +26,7 @@ function parseTelebirrMessage(message) {
 
 
 // In your utils/messageParsers.js file
-function parseCBEMessages = (message) => {
+function parseCBEMessages  (message)  {
     const transactions = [];
 
     // Regex to find the deposit amount (number followed by "Br." or "ብር")
